b6efec88d979a6b3f820322475c91c7e367d0afc
Gabriel <souzzagabe@hotmail.com>
2023-12-08 04:44:53 -0300

    finish hamburguer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import styles from "./NavBar.module.css"
 import { NavLink } from "react-router-dom"
+import { useState } from 'react'
 
 const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className={styles.navbar}>
             
             <NavLink to="/" className={styles.brand}>
                 <span className={styles.spanm}>Flogão</span>
             </NavLink>
-            <ul className={styles.links_list}>
+            <div className={styles.menu} 
+            onClick={() => setMenuOpen(!menuOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul className={`${styles.links_list} ${menuOpen ? styles.open : ""}`}>
                 <li>
                     <NavLink to="/" 
                     className={( {isActive }) => (isActive ? styles.active : "")}
