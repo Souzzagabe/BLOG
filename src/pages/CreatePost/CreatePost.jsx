a1e9f9fb6e40bbe8a92d36a27eca3157717b8009
Gabriel <souzzagabe@hotmail.com>
2023-12-13 00:15:21 -0300

    dados validation finish

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import styles from "./CreatePost.module.css"
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useState } from "react"
+import { useInsertDocument } from "../../hooks/useInsertDocument"
+import { useNavigate } from "react-router-dom"
 import { useAuthValue } from "../../context/AuthContext"
 
 const CreatePost = () => {
@@ -11,57 +11,101 @@ const CreatePost = () => {
   const [tags, setTags] = useState([])
   const [formError, setFormError] = useState("")
 
+  const { user } = useAuthValue()
+  const navigate = useNavigate()
+  const { insertDocument, response } = useInsertDocument("posts")
+
   const handleSubmit = (e) => {
     e.preventDefault()
+    setFormError("")
+
+    // Validate image
+    try {
+      new URL(image)
+    } catch (error) {
+      setFormError("The image must be a URL.")
+    }
+
+    // Create tags array
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase())
+
+    // Check values
+    if (!title || !image || !tags || !body) {
+      setFormError("Please fill in all fields!")
+    }
+
+    if (formError) return
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags: tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    })
+
+    // Redirect to the home page
+    navigate("/")
   }
 
   return (
     <div className={styles.create_post}>
       <h2>Create Post</h2>
-      <p>Write whatever you want to share, and share your knowledge.</p>
+      <p>Write about whatever you want and share your knowledge!</p>
       <form onSubmit={handleSubmit}>
         <label>
           <span>Title:</span>
-          <input type="text"
-            name='title'
+          <input
+            type="text"
+            name="text"
             required
-            placeholder='think of a good title'
+            placeholder="Think of a good title..."
             onChange={(e) => setTitle(e.target.value)}
             value={title}
           />
         </label>
         <label>
-          <span>Url:</span>
-          <input type="text"
-            name='image'
+          <span>Image URL:</span>
+          <input
+            type="text"
+            name="image"
             required
-            placeholder='insert an image'
+            placeholder="Insert an image that represents your post"
             onChange={(e) => setImage(e.target.value)}
             value={image}
           />
         </label>
         <label>
           <span>Content:</span>
-          <textarea name="body"
-            required placeholder='insert content'
+          <textarea
+            name="body"
+            required
+            placeholder="Insert the content of the post"
             onChange={(e) => setBody(e.target.value)}
             value={body}
           ></textarea>
         </label>
         <label>
-          <span>tags:</span>
-          <input type="text"
-            name='tag'
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
             required
-            placeholder='inser tag'
+            placeholder="Insert tags separated by commas"
             onChange={(e) => setTags(e.target.value)}
             value={tags}
           />
         </label>
-        <button className='btn'>Finish</button>
-        {/* {!loading && <button className='btn'>Finish</button>}
-        {loading && <button className='btn' disabled>Loading..</button>}
-        {error && <p className='error'>{error}</p>} */}
+        {!response.loading && <button className="btn">Create post!</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+            Please wait...
+          </button>
+        )}
+        {(response.error || formError) && (
+          <p className="error">{response.error || formError}</p>
+        )}
       </form>
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,24 @@
 import styles from "./Home.module.css"
 
+// hooks
+import { useNavigate, Link} from "react-router-dom"
+import { useState } from "react"
+
+// components
+
+
 const Home = () => {
+
+  
+
+
   return (
     <div className={styles.home_content}>
-        <h1>Home</h1>
+        <h1>See our most recent posts.</h1>
+        <form>
+          <input type="text" placeholder="Or search by tags..."/>
+          <button className="btn btn-dark">Search</button>
+        </form>
     </div>
   )
 }
