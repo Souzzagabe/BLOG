24de79527c1e9cfbd304df26a5674e10cb987bc6
Gabriel <souzzagabe@hotmail.com>
2023-12-15 20:15:57 -0300

    displaying individual post data made

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import styles from "./Dashboard.module.css"
 
+import {Link} from "react-router-dom"
+
+import { useAuthValue } from "../../contexts/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocument";
+
 const Dashboard = () => {
+    const {user} = useAuthValue()
+    const uid = user.uid
+    
+
     return (
         <div>
             <h1>
--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -2,15 +2,32 @@ import React from 'react'
 import styles from "./Post.module.css"
 
 import { useParams } from 'react-router-dom'
+import { useFetchDocument } from '../../hooks/useFetchDocument'
 
 const Post = () => {
     const { id } = useParams()
+    const { document: posts, loading } = useFetchDocument("posts", id)
 
-  return (
-    <div>
-        <h1>Post {id}</h1>
-    </div>
-  )
+    return (
+        <div className={styles.post_container}>
+            {loading && <h3>Loading post...</h3>}
+            {posts && (
+                <>
+                    <h1>{posts.title}</h1>
+                    <img src={posts.image} alt={posts.title} />
+                    <p>{posts.body}</p>
+                    <h3>This post is about:</h3>
+                    <div className={styles.tags}>
+                    {posts.tags.map((tag) => (
+                        <p key={tag}>
+                            <span>#</span>{tag}</p>
+
+                    ))}
+                    </div>
+                </>
+            )}
+        </div>
+    )
 }
 
 export default Post
