bb11bce4621fd574a35a22bc00bf2c6e92ebeb53
Gabriel <souzzagabe@hotmail.com>
2023-12-16 06:48:16 -0300

    build errors resolv

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import styles from '../components/PostDetail.module.css'
 import {Link} from "react-router-dom"
+import styles from "./PostDetail.module.css"
 
 const PostDetail = ({post}) => {
   return (
